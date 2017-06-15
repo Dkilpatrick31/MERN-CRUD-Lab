b39f0972e4ff5171b5c1381b3b989710eb55ca00
Dane Kilpatrick <danekilpatrick@Danes-MacBook-Air.local>
2017-06-15 13:09:30 -0500

    end of sprint 6

--- data.js
+++ data.js
@@ -1,6 +1,10 @@
 const data = [
   { id: 1, author: 'Jean', text: 'Wow this is neat!'},
-  { id: 2, author: 'Justin', text: 'youre __right!__'}
+  { id: 2, author: 'James', text: 'youre __right!__'}
+  { id: 3, author: 'Kenny', text: 'Hey guys! Dane left his laptop open again!'}
+  { id: 4, author: 'Justin', text: 'We should totally mess with it'}
+  { id: 5, author: 'Benjamin', text: 'Poor Dane, he will never learn.'}
+    
 ]
 
 module.exports = data;
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var port = process.env.API_PORT || 3001;
 
 //db config
 //ADD YOUR INFO HERE!
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds115738.mlab.com:15738/mern-comment-box');
+mongoose.connect('mongodb://dkilpatrick31:florida31@ds127842.mlab.com:27842/mern-crud');
 
 //config API to use bodyParser and look for JSON in req.body
 app.use(bodyParser.urlencoded({extended: true }));
@@ -46,7 +46,54 @@ router.route('/nuke').get(function(req,res){
 });
 
 //add /comments route to our /api router here
+router.route('/comments')
+  .get(function(req,res) {
+    console.log('Someone wants to get all comments');
+    Comment.find(function(err,comments) {
+      if (err){res.send(err);}
 
+        console.log(comments)
+      res.json(comments)
+    });
+  })
+  .post(function(req,res) {
+    var comment = new Comment();
+    comment.author = req.body.author;
+    comment.text = req.body.text;
+    comment.save(function(err) {
+      if (err)
+        res.send(err);
+      res.json({ message: 'Comment successfully added!'});
+    });
+  });
+
+router.route('/comments/:comment_id')
+//The put method gives us the chance to update our comment based on the ID passed to the route
+ .put(function(req, res) {
+   Comment.findById(req.params.comment_id, function(err, comment) {
+     if (err)
+       res.send(err);
+     //setting the new author and text to whatever was changed. If nothing was changed
+     // we will not alter the field.
+     (req.body.author) ? comment.author = req.body.author : null;
+     (req.body.text) ? comment.text = req.body.text : null;
+     //save comment
+     comment.save(function(err) {
+       if (err)
+         res.send(err);
+       res.json({ message: 'Comment has been updated' });
+     });
+   });
+ })
+ //delete method for removing a comment from our database
+ .delete(function(req, res) {
+   //selects the comment by its ID, then removes it.
+   Comment.remove({ _id: req.params.comment_id }, function(err, comment) {
+     if (err)
+       res.send(err);
+     res.json({ message: 'Comment has been deleted' })
+   })
+ });
 //use router config when we call /API
 app.use('/api', router);
 
